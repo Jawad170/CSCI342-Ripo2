943122b45db5aab45363a56825107bc6e3283b80
jawad jandali refai <jawad jandali refai>
2016-05-06 21:33:23 +0400

    GetGrades(Subject, Student) AddGrade(Subject, Student, GradedItem, Grade, Max)

--- app/src/main/java/com/csci342/justin/moodleapplication/MySQL_Handler.java
+++ app/src/main/java/com/csci342/justin/moodleapplication/MySQL_Handler.java
@@ -16,17 +16,28 @@ public class MySQL_Handler
     private static final String user = "csci342username";
     private static final String pass = "1234";
 
+    //MAIN IS USED ONLY TO TEST THE GET/SET FUNCTIONS, REMOVE FOR FINAL VERSION.
     public static void main(String[] args)
     {
+        //TEST GETTING ANNOUNCEMENTS
         String[] testAnnouncements;
         testAnnouncements = getAnnouncements("CSCI222");
-
         for ( int i = 0; i < testAnnouncements.length; i++ )
-        {
-            System.out.println(testAnnouncements[i]);
-        }
+        { System.out.println(testAnnouncements[i]); }
+
+        //TEST ADDING GRADE
+        addGrade("CSCI222", "Justin", "Deliverable 1", 4, 5);
+
+        //TEST GETTING GRADES
+        String[] testGrades;
+        testGrades = getGrades("CSCI222", "Justin");
+        for ( int i = 0; i < testGrades.length; i++ )
+        { System.out.println(testGrades[i]); }
+
+
     }
 
+    //Connects to database and returns a Statement object ready to execute sql.
     private static Statement startConnection()
     {
         //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -49,6 +60,9 @@ public class MySQL_Handler
         }
     }
 
+
+
+    //Returns in this format: [DATE] ANNOUNCEMENT
     public static String[] getAnnouncements(String Subject)
     {
         ArrayList<String> AList = new ArrayList<String>();
@@ -82,4 +96,86 @@ public class MySQL_Handler
 
     }
 
+    //Returns only announcement text
+    public static String[] getAnnouncements_NoDate(String Subject)
+    {
+        ArrayList<String> AList = new ArrayList<String>();
+        String[] announcements = null;
+
+        try
+        {
+            String sqlQuery = "SELECT * FROM `tbl_announcements` WHERE Subject = \"" + Subject + "\"";
+            ResultSet rs = startConnection().executeQuery(sqlQuery);
+
+            while (rs.next())
+            {
+                AList.add(rs.getString(2));
+            }
+
+            announcements = new String[AList.size()];
+            for ( int i = 0; i < announcements.length; i++)
+            {
+                announcements[i] = AList.get(i);
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            return announcements;
+        }
+
+    }
+
+    public static String[] getGrades(String Subject, String Student)
+    {
+
+        ArrayList<String> AList = new ArrayList<String>();
+        String[] grades = null;
+
+        try
+        {
+            String sqlQuery = "SELECT * FROM `tbl_grades` WHERE Subject = \"" + Subject + "\" AND Student = \"" + Student + "\"";
+            ResultSet rs = startConnection().executeQuery(sqlQuery);
+
+            while (rs.next())
+            {
+                AList.add("[" + rs.getString(3) + "]  " + Integer.toString(rs.getInt(4)) + " / " + Integer.toString(rs.getInt(5)));
+            }
+
+            grades = new String[AList.size()];
+            for ( int i = 0; i < grades.length; i++)
+            {
+                grades[i] = AList.get(i);
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            return grades;
+        }
+
+    }
+
+    public static void addGrade(String Subject, String Student, String GradedItem, int grade, int max)
+    {
+
+        try
+        {
+            String sqlQuery = "INSERT INTO `tbl_grades` (`Subject`, `Student`, `Graded_Item`, `Grade_Achieved`, `Grade_Max`) VALUES ('"
+            + Subject + "', '" + Student + "', '" + GradedItem + "', '" + grade + "', '" + max + "');";
+            startConnection().execute(sqlQuery);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
