e972d03a6d4a4fc7215da84475b4b892b7654430
jawad jandali refai <jawad jandali refai>
2016-05-07 12:36:34 +0400

    GetALLSubjects()
    Check if enrolled in Subject AND OR Tutorial

--- app/src/main/java/com/csci342/justin/moodleapplication/MySQL_Handler.java
+++ app/src/main/java/com/csci342/justin/moodleapplication/MySQL_Handler.java
@@ -22,17 +22,17 @@ public class MySQL_Handler
     //MAIN IS USED ONLY TO TEST THE GET/SET FUNCTIONS, REMOVE FOR FINAL VERSION.
     public static void main(String[] args)
     {
-        //......ANNOUNCEMENTS.................................................
+        //..................................................................
         ///TEST ADD ANNOUNCEMENT
         //addAnnouncement("CSCI222", "Ignore This Announcement");
 
         ///TEST GETTING ANNOUNCEMENTS
-        String[] testAnnouncements;
-        System.out.println(" ");
-        testAnnouncements = getAnnouncements("CSCI222");
-        for ( int i = 0; i < testAnnouncements.length; i++ )
-        { System.out.println(testAnnouncements[i]); }
-        //.....................................................................
+            //String[] testAnnouncements;
+            //System.out.println(" ");
+            //testAnnouncements = getAnnouncements("CSCI222");
+            //for ( int i = 0; i < testAnnouncements.length; i++ )
+            //{ System.out.println(testAnnouncements[i]); }
+        //......ANNOUNCEMENTS.................................................
 
 
         //......GRADES.........................................................
@@ -40,11 +40,11 @@ public class MySQL_Handler
         //addGrade("CSCI222", "Justin", "Deliverable 1", 4, 5);
 
         ///TEST GETTING GRADES
-        String[] testGrades;
-        System.out.println(" ");
-        testGrades = getGrades("CSCI222", "Justin");
-        for ( int i = 0; i < testGrades.length; i++ )
-        { System.out.println(testGrades[i]); }
+            //String[] testGrades;
+            //System.out.println(" ");
+            //testGrades = getGrades("CSCI222", "Justin");
+            //for ( int i = 0; i < testGrades.length; i++ )
+            //{ System.out.println(testGrades[i]); }
         //.....................................................................
 
 
@@ -53,42 +53,50 @@ public class MySQL_Handler
         //
 
         ///TEST GETS
-        System.out.println(" ");
-        System.out.println("Jawad's Hash            =  " + getPassHash("Jawad") + "\n");
-        System.out.println("Jawad's Authority       =  " + getAuthority("Jawad") + "\n");
-        System.out.println("Jawad's Token           =  " + getToken("Jawad") + "\n");
-        setToken("Jawad", 5566);
-        System.out.println("Jawad's Updated Token   =  " + getToken("Jawad") + "\n");
-        setToken("Jawad", 0);
-        System.out.println("Jawad's Reset Token     =  " + getToken("Jawad") + "\n");
+        //
+            //System.out.println(" ");
+            //System.out.println("Jawad's Hash            =  " + getPassHash("Jawad") + "\n");
+            //System.out.println("Jawad's Authority       =  " + getAuthority("Jawad") + "\n");
+            //System.out.println("Jawad's Token           =  " + getToken("Jawad") + "\n");
+            //setToken("Jawad", 5566);
+            //System.out.println("Jawad's Updated Token   =  " + getToken("Jawad") + "\n");
+            //setToken("Jawad", 0);
+            //System.out.println("Jawad's Reset Token     =  " + getToken("Jawad") + "\n");
 
-        System.out.println("Jawad's PersonalInfo:  ");
-        String[] PI = getPersonalInfo("Jawad");
-        System.out.println(PI[0]);
-        System.out.println(PI[1]);
-        System.out.println(PI[2]);
-        System.out.println(PI[3] + "\n");
+            //System.out.println("Jawad's PersonalInfo:  ");
+            //String[] PI = getPersonalInfo("Jawad");
+            //System.out.println(PI[0]);
+            //System.out.println(PI[1]);
+            //System.out.println(PI[2]);
+            //System.out.println(PI[3] + "\n");
 
-        setPersonalInfo("Jawad", "Bobo", "Bobberson", "Teacher", "Bob@Bobmail.com", "+1-247-5551337", "Lala Land");
-        System.out.println("Jawad's Updated PersonalInfo:  ");
-        PI = getPersonalInfo("Jawad");
-        System.out.println(PI[0]);
-        System.out.println(PI[1]);
-        System.out.println(PI[2]);
-        System.out.println(PI[3] + "\n");
+            //setPersonalInfo("Jawad", "Bobo", "Bobberson", "Teacher", "Bob@Bobmail.com", "+1-247-5551337", "Lala Land");
+            //System.out.println("Jawad's Updated PersonalInfo:  ");
+            //PI = getPersonalInfo("Jawad");
+            //System.out.println(PI[0]);
+            //System.out.println(PI[1]);
+            //System.out.println(PI[2]);
+            //System.out.println(PI[3] + "\n");
 
 
-        setPersonalInfo("Jawad", "Jawad", "Jandali Refai", "Student", "jjr318@uowmail.edu.au", "+971-50-1620709", "UOWD, Knowledge Village, Dubai");
-        System.out.println("Jawad's Reset PersonalInfo:  ");
-        PI = getPersonalInfo("Jawad");
-        System.out.println(PI[0]);
-        System.out.println(PI[1]);
-        System.out.println(PI[2]);
-        System.out.println(PI[3] + "\n");
+            //setPersonalInfo("Jawad", "Jawad", "Jandali Refai", "Student", "jjr318@uowmail.edu.au", "+971-50-1620709", "UOWD, Knowledge Village, Dubai");
+            //System.out.println("Jawad's Reset PersonalInfo:  ");
+            //PI = getPersonalInfo("Jawad");
+            //System.out.println(PI[0]);
+            //System.out.println(PI[1]);
+            //System.out.println(PI[2]);
+            //System.out.println(PI[3] + "\n");
 
         //.....................................................................
 
+        ///TESTING GET ALL SUBJECTS
+            //String Subjects[] = getALLSujects();
+            //for (int i = 0; i < Subjects.length; i++ )
+            //{ System.out.println(Subjects[i]); }
+        //.....................................................................
 
+        System.out.println("\n Is Justin Enrolled in CSCI222? " + IsEnrolledInSubject("Justin", "CSCI222"));
+        System.out.println("\n Is Justin Enrolled in it's Tutorial? " + IsEnrolledInTutorial("Justin", "CSCI222"));
     }
 
     //Connects to database and returns a Statement object ready to execute sql.
@@ -368,6 +376,7 @@ public class MySQL_Handler
         }
     }
 
+    //Updates entry in database for specified username with provided details (all strings)
     public static boolean setPersonalInfo(String Username , String FirstName, String LastName,
                                           String Authority, String Email    , String Phone   , String Address  )
     {
@@ -390,7 +399,94 @@ public class MySQL_Handler
             return false;
         }
     }
-    //UpdatePersonalInfo
+
+    //Returns array of ALL subjects possible (CSCI222, ARTS015, etc...)
+    public static String[] getALLSujects()
+    {
+        ArrayList<String> AList = new ArrayList<String>();
+        String[] subjects = null;
+
+        try
+        {
+            String sqlQuery = "SELECT * FROM `tbl_listofsubjects` ORDER BY `Subject` ASC";
+            ResultSet rs = startConnection().executeQuery(sqlQuery);
+
+            while (rs.next())
+            {
+                AList.add(rs.getString(1));
+            }
+
+            subjects = new String[AList.size()];
+            for ( int i = 0; i < subjects.length; i++)
+            {
+                subjects[i] = AList.get(i);
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            return subjects;
+        }
+    }
+
+    //Returns TRUE if provided student name is enrolled in specified subject
+    public static boolean IsEnrolledInSubject(String Student, String Subject)
+    {
+        int type = -1;
+
+        try
+        {
+            String sqlQuery = "SELECT Type FROM `tbl_enrollments` WHERE Student = \"" + Student + "\" AND Enrolled = \"" + Subject + "\"";
+            ResultSet rs = startConnection().executeQuery(sqlQuery);
+
+            while (rs.next())
+            {
+                int newType = rs.getInt(1);
+                if ( newType == 1 ) type = 1;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if ( type == 1) return true;
+            else return false;
+        }
+    }
+
+    //Returns TRUE if provided student name is enrolled in specified subject tutorial
+    public static boolean IsEnrolledInTutorial(String Student, String Subject)
+    {
+        int type = -1;
+
+        try
+        {
+            String sqlQuery = "SELECT Type FROM `tbl_enrollments` WHERE Student = \"" + Student + "\" AND Enrolled = \"" + Subject + "\"";
+            ResultSet rs = startConnection().executeQuery(sqlQuery);
+
+            while (rs.next())
+            {
+                int newType = rs.getInt(1);
+                if ( newType == 2 ) type = 2;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if ( type == 2) return true;
+            else return false;
+        }
+    }
+
 
 
     public static String getToday()
